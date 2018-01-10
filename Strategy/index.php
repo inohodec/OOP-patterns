ed857e12c7bb449185621ef55cf32eb2e0e1c42b
inohodec <judas.box@gmail.com>
2018-01-10 20:40:44 +0200

    first real changing

--- Strategy/index.php
+++ Strategy/index.php
@@ -1,6 +1,29 @@
 <?php
 
-class Duck
-{
-    private $color;
+abstract class Duck {
+    function swim() {
+        echo "I can swim";
+    }
+    function quack() {
+        echo "I can quack";
+    }
+    abstract function fly();
+}
+
+class RedDuck extends Duck {
+    public function fly() {
+        echo "I use my wings for flying";
+    }
+}
+
+class GreyDuck extends Duck {
+    public function fly() {
+        echo "I use my wings for flying";
+    }
+}
+
+class WoodenDuck extends Duck {
+    public function fly() {
+        echo "I can't fly";
+    }
 }
