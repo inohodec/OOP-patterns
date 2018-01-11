965e66665056602f487c493f5ae3f8f6cb014966
inohodec <judas.box@gmail.com>
2018-01-11 13:46:05 +0200

    Added style for menu on main page

--- index.php
+++ index.php
@@ -3,8 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <title>PHP OOP-patterns</title>
+    <style>
+        .list {
+            max-width: 400px;
+            border: 3px solid grey;
+            padding: 20px;
+            background-color: beige;
+        }
+
+        .list a {
+            display: inline-block;
+            border-bottom: 1px dashed grey;
+        }
+    </style>
 </head>
 <body>
+<div class="list">
     <a href="Strategy/index.php">Strategy Pattern</a>
+</div>
+
 </body>
 </html>
