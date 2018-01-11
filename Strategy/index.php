cd3d7ca78532dcf9536b170634ed0964bb159a25
inohodec <judas.box@gmail.com>
2018-01-11 12:36:28 +0200

    I've added index.php with links on Patterns

--- Strategy/index.php
+++ Strategy/index.php
@@ -13,18 +13,25 @@ abstract class SpaceShip {
         echo "I can shoot as all military Spaceship<br>";
     }
 
-    abstract function display();
+    abstract function description();
+
+    function display() {
+        $this->swim();
+        $this->shoot();
+        $this->description();
+        echo "<hr>";
+    }
 }
 
 class Dreadnought extends SpaceShip {
-    function display()
+    function description()
     {
         echo "I'm very big and powerful SpaseShip with name ".__CLASS__."<br>";
     }
 }
 
 class Scout extends SpaceShip {
-    function display()
+    function description()
     {
         echo "I'm small, inconspicuous and rapid SpaceShip with name ".__CLASS__."<br>";
     }
@@ -35,8 +42,16 @@ class ToyShip extends SpaceShip {
     {
         echo "Unfortunately I can't shoot I'm only a toy (((";
     }
-    function display()
+    function description()
     {
         echo "I'm a toy SpaceShip";
     }
 }
+
+$ship1 = new Dreadnought();
+$ship2 = new Scout();
+$ship3 = new ToyShip();
+
+$ship1->display();
+$ship2->display();
+$ship3->display();
